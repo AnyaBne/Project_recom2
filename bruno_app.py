5a3e48e3b56e8494746e7b84b33dcd47ca626a98
AnyaBne <49951220+AnyaBne@users.noreply.github.com>
2023-12-06 00:02:13 +0100

    Update bruno_app.py

--- bruno_app.py
+++ bruno_app.py
@@ -44,9 +44,9 @@ def find_similar_users(user_id):
 
 def explain_content_based_selection(selected_songs, recommended_songs_df, tfidf_vectorizer):
     explanations = []
-    
+    df['combined_attributes2'] = 'title: '+ df['title'] + ' ' + 'release: '+ df['release'] + ' ' + 'artist name: ' + df['artist_name'] + ' ' + 'year: ' df['year'].astype(str)
     for song in selected_songs:
-        selected_song_attributes = df[df['title'] == song]['combined_attributes'].iloc[0]
+        selected_song_attributes = df[df['title'] == song]['combined_attributes2'].iloc[0]
         explanation = f"Selected song '{song}' has these key attributes: {selected_song_attributes}."
         explanations.append(explanation)
 
